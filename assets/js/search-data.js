961df408a1373c0eea94911bf0cc052e3afde932
huhady <hazem.usama.said@gmail.com>
2025-05-03 17:07:43 +0000

    Deploying to gh-pages from @ huhady/huhady.github.io@b3fbb065e1b93ab4152cb4e68ce3693fab43e8fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-i-am-excited-to-be-joining-texas-a-amp-amp-m-the-geography-department-as-an-assistant-professor-in-fall-2025",
+          title: 'I am excited to be joining Texas A&amp;amp;amp;M, the Geography Department as an...',
+          description: "",
+          section: "News",},{id: "news-phd-students-spring-2026-fall2026",
+          title: 'PhD Students Spring 2026/Fall2026',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
