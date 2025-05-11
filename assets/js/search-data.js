25d415cb0fb9e0e147748fa5b5d4e1a67883ae2f
huhady <hazem.usama.said@gmail.com>
2025-05-11 16:16:15 +0000

    Deploying to gh-pages from @ huhady/huhady.github.io@6b1864226d2fbf4cc2f79250c633486cd1ba8198 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -50,9 +50,9 @@ ninja.data = [{
           description: "Started in 2021",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-the-great-lakes-shoreline-changes-model-glsm",
+          title: 'The Great Lakes shoreline changes model (GLSM)',
+          description: "Started in 2021",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
